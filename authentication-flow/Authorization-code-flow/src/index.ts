44a0e787bc2df225fc02dc29ae319a28cccb5142
Alekseidasilva <alekseidasilva@hotmail.com>
2024-04-05 12:05:07 +0100

    :ab: Trocando um Authorization Code pelos tokens
    :ab: Trocando um Authorization Code pelos tokens

--- authentication-flow/Authorization-code-flow/src/index.ts
+++ authentication-flow/Authorization-code-flow/src/index.ts
@@ -6,7 +6,7 @@ app.get('/login',(req,res)=>{
 
     const loginParams=new URLSearchParams({
         client_id:'fullcycle-client',
-        redirect_uri:'http://localhost:8080/callback',
+        redirect_uri:'http://localhost:3000/callback',
         response_type:'code',
         scope:'openid'
     });
@@ -14,6 +14,32 @@ app.get('/login',(req,res)=>{
     console.log(url);
     res.redirect(url);
 });
+
+
+app.get('/callback',async(req,res)=>{
+
+    console.log(req.query);
+
+    const bodyParams=new URLSearchParams({
+        client_id:'fullcycle-client',
+        grant_type:'authorization_code',
+        code:req.query.code as string,
+        redirect_uri:'http://localhost:3000/callback'
+    });
+    const url=`http://localhost:8080/realms/fullcycle-realm/protocol/openid-connect/token`;
+   const respponse=  await fetch(url,{
+        method:'POST',
+        headers:{
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body:bodyParams.toString()
+    });   
+    const result=await respponse.json();
+    
+   console.log(result);
+   res.json(result);
+});
+
 
 app.listen(3000,()=>{
     console.log('Listening on port 3000');
